ba10c6bccc49079cae594462b6964a820e415e20
cshablin <73110953+cshablin@users.noreply.github.com>
2021-03-16 14:34:00 +0200

    Test + NN files

---
+++ NeuralNetTest.py
@@ -0,0 +1,50 @@
+import unittest
+import numpy as np
+
+from DL_1.NeuralNetwork import Network, Layer
+
+
+class NeuralNetworkTests(unittest.TestCase):
+
+    def test_init_layers(self):
+
+        network = Network()
+        layers_dims = [2, 3, 4]
+        layers_params = network.initialize_parameters(layers_dims)
+        last_layer = layers_params[2]
+        self.assertEqual((4, 3), last_layer.weights.shape)
+
+    def test_net_forward_pass(self):
+
+        network = Network()
+        layers_dims = [2, 3, 4]
+        network.initialize_parameters(layers_dims)
+        x = np.array([[5.0, 3.0],
+                      [1.0, 4.0]])
+        result = network.linear_model_forward(x)
+        self.assertEqual((4, 3), result)
+
+    def test_linear_forward(self):
+        layer = Layer(None, None)
+        w = np.array([[5.0, 1.0, 3.0],
+                      [1.0, 2.0, 1.0]])
+
+        bias = np.array([[7.0],
+                         [1.0]])
+
+        activations = np.array([1.0, 1.0, 1.0])
+
+        result = layer.linear_forward(activations, w, bias)
+        self.assertTrue(np.array_equal(result, np.array([16.0, 5.0])))
+
+    def test_soft_max(self):
+        layer = Layer(None, None)
+        z = np.array([2.0, 2.0, 2.0, 2.0])
+        result = layer.soft_max(z)
+        self.assertTrue(np.array_equal(result, np.array([0.25, 0.25, 0.25, 0.25])))
+
+    def test_relu(self):
+        layer = Layer(None, None)
+        z = np.array([-2.0, 2.0, -2.0, 2.0])
+        result = layer.relu(z)
+        self.assertTrue(np.array_equal(result, np.array([0, 2.0, 0, 2.0])))
---
+++ NeuralNetwork.py
@@ -0,0 +1,116 @@
+from typing import List, Dict
+import numpy as np
+
+
+class Layer(object):
+
+    def __init__(self, weights: np.array, bias: np.array):
+        self.weights = weights
+        self.bias = bias
+        self.activation_cache = None
+        self.previous_linear_activation_cache = None
+
+    def linear_forward(self, activation: np.array, weights: np.array, bias: np.array) -> np.array:
+        '''
+
+        :param activation: the activations of the previous layer
+        :param weights: the weight matrix of the current layer (shape [size of current layer, size of previous layer])
+        :param bias: bias vector of the current layer (shape [size of current layer, 1])
+        :return: the linear component of the activation function (the value before applying the non-linear function)
+        '''
+        self.previous_linear_activation_cache = activation
+        self.weights = weights
+        self.bias = bias
+
+        matrix = np.concatenate((weights, bias), axis=1)
+        activation_extended = np.insert(activation, len(activation), 1.0, axis=0)
+        return matrix.dot(activation_extended)
+
+    # def soft_max(self, z: np.array):
+    #     '''
+    #
+    #     :param z: the linear component of the activation function
+    #     :return: activations of the layer
+    #     '''
+    #     self.activation_cache = z
+    #     z_exp = np.exp(z)
+    #     sum_z_exp = np.sum(z_exp)
+    #     return z_exp / sum_z_exp
+
+    def soft_max(self, z: np.array):
+        s = np.max(z, axis=1)
+        s = s[:, np.newaxis]  # broadcasting
+        e_x = np.exp(z - s)
+        div = np.sum(e_x, axis=1)
+        div = div[:, np.newaxis]
+        return e_x / div
+
+    def relu(self, z: np.array) -> np.array:
+        '''
+
+        :param z: the linear component of the activation function
+        :return: activations of the layer
+        '''
+        self.activation_cache = z
+        # result = np.maximum(np.zeros(len(z)), z)
+        result = np.maximum(np.zeros(z.shape), z)
+        return result
+
+    def linear_activation_forward(self, activations_prev: np.array, weights: np.array, bias: np.array,
+                                  activation: str) -> np.array:
+        '''
+
+        :param activations_prev: activations of the previous layer
+        :param weights: the weight matrix of the current layer
+        :param bias: bias vector of the current layer
+        :param activation: activation function to be used (“softmax” or “relu”)
+        :return: activations of the current layer
+        '''
+        z = self.linear_forward(activations_prev, weights, bias)
+        if activation == "relu":
+            return self.relu(z)
+        if activation == "softmax":
+            return self.soft_max(z)
+
+
+class Network(object):
+
+    def __init__(self):
+        self.index_2_layer = {}
+        self.last_layer = None
+
+    def initialize_parameters(self, layer_dims: List[int]) -> Dict[int, Layer]:
+        '''
+
+        :param layer_dims: an array of the dimensions of each layer in the network
+        :return: a dictionary containing the initialized W and b parameters of each layer (W1…WL, b1…bL).
+        '''
+
+        index_2_layer = {}
+        for layer in range(1, len(layer_dims), 1):
+            weight_layer_shape = (layer_dims[layer], layer_dims[layer - 1])
+            weight_matrix = np.zeros(weight_layer_shape) + np.random.uniform(-1.0, 1.0, weight_layer_shape)
+            bias_shape = (layer_dims[layer], 1)
+            bias_vector = np.zeros(bias_shape) + np.random.uniform(-1.0, 1.0, bias_shape)
+            index_2_layer[layer] = Layer(weight_matrix, bias_vector)
+
+        self.index_2_layer = dict(index_2_layer)
+        self.last_layer = self.index_2_layer.popitem()[1]
+        return index_2_layer
+
+    def linear_model_forward(self, x: np.array, use_batchnorm: bool = False) -> np.array:
+        """
+
+        :param x: the data, numpy array of shape (input size, number of examples)
+        :param parameters: the initialized W and b parameters of each layer
+        :param use_batchnorm: boolean flag used to determine whether to apply batchnorm after the activation
+        :return: last post-activation value
+        """
+        prev_activations = x
+        for index, layer in self.index_2_layer.items():
+            prev_activations = layer.linear_activation_forward(prev_activations, layer.weights, layer.bias, "relu")
+
+        result = self.last_layer.linear_activation_forward(prev_activations, self.last_layer.weights, self.last_layer.bias, "softmax")
+        return result
+
+
