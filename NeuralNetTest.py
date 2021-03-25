8e9e11cf7a87c7c4c5d83b68daac2c6438c8d067
cshablin <Carmel.Shablin@kla-tencor.com>
2021-03-25 22:24:55 +0200

    test_neural_net_regular()

--- NeuralNetTest.py
+++ NeuralNetTest.py
@@ -133,6 +133,7 @@ class NeuralNetTests(unittest.TestCase):
 
 
 class ReportTests(unittest.TestCase):
+    # we use same test train sets for all 3 runs
     (x_train, y_train), (x_test, y_test) = load_data()
     y_train = one_hot(y_train, 10)
     y_test = one_hot(y_test, 10)
@@ -143,7 +144,10 @@ class ReportTests(unittest.TestCase):
         layers_dims = [image_size, 20, 7, 5, 10]
         params, costs = L_layer_model(self.x_train.T, self.y_train.T, layers_dims, num_iterations=1000, batch_size=64, learning_rate=0.009)
         test_score = predict(self.x_test.T, self.y_test.T, params)
-        print("score-", test_score)
+        train_score = predict(self.x_train.T, self.y_train.T, params)
+        print("final train score-", train_score)
+        print("final validation score-", test_score)
+        print("final test score-", test_score)
 
     def test_neural_net_batch_norm(self):
         pass
