2ad3ba2cb283242056d2b51f7f45ce88a37d0e91
cshablin <Carmel.Shablin@kla-tencor.com>
2021-04-14 16:31:49 +0300

    best result for now ac 0.65 with Xavier initialization

--- Assignment_2/OneShotModel.py
+++ Assignment_2/OneShotModel.py
@@ -1,5 +1,5 @@
 from keras.callbacks import History
-from tensorflow.keras import models, optimizers, losses, activations, callbacks
+from tensorflow.keras import models, optimizers, losses, activations, callbacks, initializers
 from tensorflow.keras.layers import *
 import tensorflow.keras.backend as backend
 import tensorflow as tf
@@ -7,74 +7,30 @@ import tensorflow as tf
 
 class OneShotModel(object):
 
-    # def __init__( self ):
-    #
-    #
-    #     self.__DIMEN = 250
-    #
-    #     input_shape = ( (self.__DIMEN**2) * 1 , )
-    #     convolution_shape = ( self.__DIMEN , self.__DIMEN, 1)
-    #     kernel_size_1 = ( 4 , 4 )
-    #     kernel_size_2 = ( 3 , 3 )
-    #     pool_size_1 = ( 3 , 3 )
-    #     pool_size_2 = ( 2 , 2 )
-    #     strides = 1
-    #
-    #     seq_conv_model = [
-    #
-    #         Reshape( input_shape=input_shape , target_shape=convolution_shape),
-    #
-    #         Conv2D( 32, kernel_size=kernel_size_1 , strides=strides , activation=activations.relu ),
-    #         Conv2D( 32, kernel_size=kernel_size_1, strides=strides, activation=activations.relu),
-    #         MaxPooling2D(pool_size=pool_size_1, strides=strides ),
-    #
-    #         Conv2D( 64, kernel_size=kernel_size_2 , strides=strides , activation=activations.relu ),
-    #         Conv2D( 64, kernel_size=kernel_size_2 , strides=strides , activation=activations.relu ),
-    #         MaxPooling2D(pool_size=pool_size_2 , strides=strides),
-    #
-    #         Flatten(),
-    #
-    #         Dense( 64 , activation=activations.sigmoid )
-    #
-    #     ]
-    #
-    #     seq_model = tf.keras.Sequential( seq_conv_model )
-    #
-    #     input_x1 = Input( shape=input_shape )
-    #     input_x2 = Input( shape=input_shape )
-    #
-    #     output_x1 = seq_model( input_x1 )
-    #     output_x2 = seq_model( input_x2 )
-    #
-    #     distance_euclid = Lambda( lambda tensors : backend.abs( tensors[0] - tensors[1] ))( [output_x1 , output_x2] )
-    #     outputs = Dense( 1 , activation=activations.sigmoid) ( distance_euclid )
-    #     self.__model = models.Model( [ input_x1 , input_x2 ] , outputs )
-    #
-    #     self.__model.compile( loss=losses.binary_crossentropy , optimizer=optimizers.Adam(lr=0.0001))
-    #     print(self.__model.summary())
-
     def __init__(self):
         self.__dim = 250
         input_shape = (self.__dim ** 2,)
         convolution_shape = (self.__dim, self.__dim, 1)
         strides = 1
-
         seq_conv_model = [
 
             Reshape(input_shape=input_shape, target_shape=convolution_shape),
-            Conv2D(32, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same"),
-            Conv2D(32, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same"),
+            Conv2D(32, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
+            Conv2D(32, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
             # MaxPooling2D(pool_size=(2, 2), strides=strides),
             MaxPooling2D(pool_size=(2, 2), strides=2),
-            Conv2D(64, kernel_size=(3, 3), strides=1, activation=activations.relu),
-            Conv2D(64, kernel_size=(3, 3), strides=1, activation=activations.relu),
+            Conv2D(64, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
+            Conv2D(64, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
             MaxPooling2D(pool_size=(2, 2), strides=2),
-            Conv2D(128, kernel_size=(3, 3), strides=1, activation=activations.relu),
-            Conv2D(128, kernel_size=(3, 3), strides=1, activation=activations.relu),
+            Conv2D(128, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
+            Conv2D(128, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.glorot_normal),
+            MaxPooling2D(pool_size=(2, 2), strides=2),
+            Conv2D(256, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.RandomNormal(stddev=0.01)),
+            Conv2D(256, kernel_size=(3, 3), strides=1, activation=activations.relu, padding="same", kernel_initializer=initializers.RandomNormal(stddev=0.01)),
             MaxPooling2D(pool_size=(2, 2), strides=2),
             Flatten(),
-            Dense(1024, activation=activations.sigmoid),
-            Dense(1024, activation=activations.sigmoid)
+            Dense(1024, activation=activations.relu, kernel_initializer=initializers.RandomNormal(mean=0, stddev=0.01)),
+            Dense(1024, activation=activations.sigmoid, kernel_initializer=initializers.RandomNormal(mean=0, stddev=0.01))
         ]
 
         seq_model = tf.keras.Sequential(seq_conv_model)
@@ -89,18 +45,20 @@ class OneShotModel(object):
         outputs = Dense(1, activation=activations.sigmoid)(distance_euclid)
         self.__model = models.Model([input_x1, input_x2], outputs)
 
-        self.__model.compile(loss=losses.binary_crossentropy, optimizer=optimizers.Adam(lr=0.0001))
+        self.__model.compile(loss=losses.binary_crossentropy, optimizer=optimizers.Adam(lr=0.0001), metrics=['accuracy'])
         print(self.__model.summary())
 
     def fit(self, x, y, hyper_parameters):
         # This callback will stop the training when there is no improvement in
         # the validation loss for three consecutive epochs.
         callback = callbacks.EarlyStopping(monitor='loss', patience=3)
+        # callback = callbacks.EarlyStopping(monitor='val_accuracy', mode='max', min_delta=1)
         history = self.__model.fit(x, y, batch_size=hyper_parameters['batch_size'], epochs=hyper_parameters['epochs'],
                                    callbacks=[callback], validation_split=hyper_parameters['validation_split'], verbose=2)
-                                   #validation_data=hyper_parameters['val_data'])
+        #validation_data=hyper_parameters['val_data'])
 
         self.__model.summary()
+        return history
 
     def predict(self, x):
         predictions = self.__model.predict(x)
