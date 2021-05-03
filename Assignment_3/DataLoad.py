4799212325964b6f393cb8ba524d6febce777b9e
cshablin <Carmel.Shablin@kla-tencor.com>
2021-05-04 00:09:55 +0300

    minor data load

--- Assignment_3/DataLoad.py
+++ Assignment_3/DataLoad.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from pandas import DataFrame
 
 
-def load_lyrics(csv_path: str):
+def load_lyrics(csv_path: str) -> DataFrame:
     df = pd.read_csv(csv_path, sep='\n', header=None)
     res = df.iloc[:, 0].str.rstrip(r'&, ').str.extract(r'([^,]+),([^,]+),(.+)')
     res.columns = ['artist', 'title', 'lyrics']
